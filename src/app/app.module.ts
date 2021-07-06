87cdb988bc2f8cfc3a9667a4c903fb785a70a119
Vishnu Vinod <vishnuvinod2772001@gmail.com>
2021-07-06 16:54:21 +0530

    working qr scanner with jsrq in first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { SecondComponent } from './second/second.component';
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
+import { HomeComponent } from './home/home.component';
+import { FirstComponent } from './first/first.component';
+import { ThreeComponent } from './three/three.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'first', component: FirstComponent },
+  { path: 'second', component: SecondComponent },
+  { path: 'three', component: ThreeComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SecondComponent,
+    HomeComponent,
+    FirstComponent,
+    ThreeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      routes
+    ),
+    ZXingScannerModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent], 
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
---
+++ src/app/first/first.component.ts
@@ -0,0 +1,77 @@
+import { ElementRef } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
+import jsQR from "jsqr";
+
+@Component({
+  selector: 'app-first',
+  templateUrl: './first.component.html',
+  styleUrls: ['./first.component.scss']
+})
+export class FirstComponent {
+  WIDTH = 640;
+  HEIGHT = 480;
+
+  @ViewChild("video")
+  public video: ElementRef;
+
+  @ViewChild("canvas")
+  public canvas: ElementRef;
+
+  // captures: string[] = [];
+  error: any;
+  isCaptured: boolean = false;
+  stream: any;
+  timer: any;
+
+  async ngAfterViewInit() {
+    await this.setupDevices();
+  }
+
+  stop() {
+    clearTimeout(this.timer)
+    this.video.nativeElement.pause();
+    this.stream.getTracks().forEach(function (track) {
+      track.stop();
+    });
+  }
+
+  async setupDevices() {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      try {
+        this.stream = await navigator.mediaDevices.getUserMedia({
+          video: { facingMode: "environment" }
+        });
+        if (this.stream) {
+          this.video.nativeElement.srcObject = this.stream;
+          this.video.nativeElement.play();
+          this.error = null;
+          this.checkImage();
+        } else {
+          this.error = "You have no output video device";
+        }
+      } catch (e) {
+        this.error = e;
+      }
+    }
+  }
+
+  checkImage() {
+    const WIDTH = this.video.nativeElement.clientWidth;
+    const HEIGHT = this.video.nativeElement.clientHeight;
+    this.canvas.nativeElement.width = WIDTH;
+    this.canvas.nativeElement.height = HEIGHT;
+    const ctx = this.canvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
+    ctx.drawImage(this.video.nativeElement, 0, 0, WIDTH, HEIGHT)
+    const imageData = ctx.getImageData(0, 0, WIDTH, HEIGHT)
+    const code = jsQR(imageData.data, imageData.width, imageData.height, { inversionAttempts: "dontInvert" })
+
+    if (code) {
+      this.stop();
+      alert(code.data);
+    } else {
+      this.timer = setTimeout(() => { this.checkImage(); }, 100)
+    }
+  }
+
+}
